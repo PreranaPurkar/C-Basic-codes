72489fecbad3e6d91c1b6b89316c758590c29956
PreranaPurkar <preranapurkar12@gmail.com>
2023-01-31 20:54:04 +0530

    [ADDED]:[Prerana] Addition of two number in c

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,add=0;
+	clrscr();
+	printf("Enter the no of a and b");
+	scanf("%d%d",&a,&b);
+	add=a+b;
+	printf("Addition of two number is %d ",add);
+	getch();
+}
