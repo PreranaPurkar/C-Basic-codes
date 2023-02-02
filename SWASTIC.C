4bf705fc5d661edfeeeba8fa32b85114189b9dfa
PreranaPurkar <preranapurkar12@gmail.com>
2023-02-02 16:50:27 +0530

    [ADDED]:[Prerana] Draw Swastic by using *

---
+++ SWASTIC.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("\n*\t*********");
+	printf("\n*\t*");
+	printf("\n*\t*");
+	printf("\n*\t*");
+	printf("\n*\t*");
+	printf("\n*****************");
+	printf("\n\t*\t*");
+	printf("\n\t*\t*");
+	printf("\n\t*\t*");
+	printf("\n\t*\t*");
+	printf("\n*********\t*");
+	getch();
+}
